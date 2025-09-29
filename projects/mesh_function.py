7f430506b397f45bf86f3d6da746f025ecb7b062
FrancescoMinisini <fg.minisini@gmail.com>
2025-09-29 12:59:37 +0200

    Fixed professors error

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,7 +2,7 @@ import numpy as np
 from collections.abc import Callable
 
 
-def mesh_function(f: Callable[[float], float], t: np.ndarray) -> np.ndarray:
+def mesh_function(f: Callable[[float | np.ndarray ], float | np.ndarray], t: np.ndarray) -> np.ndarray:
     return  f(t)
      
 
@@ -13,7 +13,7 @@ def func(t: float | np.ndarray) -> float | np.ndarray:
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
-    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-4)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
 
