04f1f2a6ef0d81bf2a35725ed2f005d426942301
FrancescoMinisini <fg.minisini@gmail.com>
2025-10-01 14:39:40 +0200

    Gooooo

--- projects/Wave1D.py
+++ projects/Wave1D.py
@@ -65,8 +65,14 @@ class Wave1D:
         left =bc["left"]
         right = bc["right"]
         if left == 3 and right == 3:
-            D[0, -1] = 1.0
-            D[-1, 0] = 1.0
+            D[0, :] = 0.0
+            D[0, 0] = -2.0
+            D[0, 1] = 1.0
+
+            D[-1 ,:] = 0.0
+            D[-1 ,-1] = -2.0
+            D[-1 ,-2] = 1.0
+
         elif left == 3 or right == 3:
             raise RuntimeError(f"Periodic bound cond cannot be mixed bc = [left:{left}, right:{right}]")
         elif left == 0:
@@ -78,7 +84,9 @@ class Wave1D:
             pass
         else: raise RuntimeError(f"Wrong bc =  [left : {left} , right : {right}]")
         
-        if right == 0:
+        if right == 3:
+            pass
+        elif right == 0:
             D[-1, :] = 0.0
         elif right == 1:
             D[-1, : ] = 0.0
