de7744c6d2a79ba3ae02ae75c1e52d91ad561e06
FrancescoMinisini <fg.minisini@gmail.com>
2025-10-06 20:52:01 +0200

    Alè

--- projects/poisson2d.py
+++ projects/poisson2d.py
@@ -16,21 +16,41 @@ class Poisson2D:
     """
 
     def __init__(self, Lx, Ly, Nx, Ny):
-        self.px = Poisson(Lx, Nx) # we can reuse some of the code from the 1D case
+        self.px = Poisson(Lx, Nx)
         self.py = Poisson(Ly, Ny)
 
     def create_mesh(self):
         """Return a 2D Cartesian mesh
         """
-        raise NotImplementedError
+        x = np.linspace(0, self.px.L, self.px.N+1)
+        y = np.linspace(0, self.py.L, self.py.N+1)
+        smesh = np.meshgrid(x, y, indexing='ij')
+        return smesh
 
     def laplace(self):
         """Return a vectorized Laplace operator"""
-        raise NotImplementedError
+        D2X = self.px.D2()
+        D2Y = self.py.D2()
+        return (sparse.kron(D2X, sparse.eye(self.py.N+1)) + sparse.kron(sparse.eye(self.px.N+1), D2Y))
 
     def assemble(self, f=None):
         """Return assembled coefficient matrix A and right hand side vector b"""
-        raise NotImplementedError
+        xij, yij = self.create_mesh()
+        F = sp.lambdify((x, y), f)(xij, yij)
+        A = self.laplace()
+        B = np.ones((self.px.N+1, self.py.N+1), dtype=bool)
+        B[1:-1, 1:-1] = 0
+        bnds = np.where(B.ravel() == 1)[0]
+        A = A.tolil()
+        for i in bnds:
+            A[i] = 0
+            A[i, i] = 1
+        A = A.tocsr()
+        
+        b = F.ravel()
+        b[bnds] = 0
+
+        return A , b 
 
     def l2_error(self, u, ue):
         """Return l2-error
@@ -42,7 +62,9 @@ class Poisson2D:
         ue : Sympy function
             The analytical solution
         """
-        raise NotImplementedError
+        xij, yij = self.create_mesh()
+        return np.sqrt(self.px.dx*self.py.dx*np.sum((u - sp.lambdify((x, y), ue)(xij, yij))**2))
+
 
     def __call__(self, f=implemented_function('f', lambda x, y: 2)(x, y)):
         """Solve Poisson's equation with a given right hand side function
@@ -61,5 +83,11 @@ class Poisson2D:
         return sparse.linalg.spsolve(A, b.ravel()).reshape((self.px.N+1, self.py.N+1))
 
 def test_poisson2d():
-    assert False
+        um = x*(1-x)*y*(1-y)*sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
+        f = um.diff(x, 2) + um.diff(y, 2)
+        solver = Poisson2D(1, 1, 100, 100)
+        u = solver(f)
+        error = solver.l2_error(u, um)
+        assert error < 5e-4, f"Error on finest grid too large: {error}"
+
 
