c1f9c69473652e647233f9d1fb15f84f86392e2f
FrancescoMinisini <fg.minisini@gmail.com>
2025-10-06 21:06:20 +0200

    Completed lesson 6

--- projects/poisson2d.py
+++ projects/poisson2d.py
@@ -33,14 +33,21 @@ class Poisson2D:
         D2Y = self.py.D2()
         return (sparse.kron(D2X, sparse.eye(self.py.N+1)) + sparse.kron(sparse.eye(self.px.N+1), D2Y))
 
-    def assemble(self, f=None):
+    def assemble(self, f=None, g=None):
         """Return assembled coefficient matrix A and right hand side vector b"""
         xij, yij = self.create_mesh()
         F = sp.lambdify((x, y), f)(xij, yij)
+        
+        if g is None:
+            G = np.zeros_like(F)
+        else:
+            G = sp.lambdify((x, y), g)(xij, yij)
+
         A = self.laplace()
         B = np.ones((self.px.N+1, self.py.N+1), dtype=bool)
         B[1:-1, 1:-1] = 0
         bnds = np.where(B.ravel() == 1)[0]
+
         A = A.tolil()
         for i in bnds:
             A[i] = 0
@@ -48,7 +55,7 @@ class Poisson2D:
         A = A.tocsr()
         
         b = F.ravel()
-        b[bnds] = 0
+        b[bnds] = G.ravel()[bnds]
 
         return A , b 
 
@@ -66,7 +73,7 @@ class Poisson2D:
         return np.sqrt(self.px.dx*self.py.dx*np.sum((u - sp.lambdify((x, y), ue)(xij, yij))**2))
 
 
-    def __call__(self, f=implemented_function('f', lambda x, y: 2)(x, y)):
+    def __call__(self, f=implemented_function('f', lambda x, y: 2)(x, y), g=None):
         """Solve Poisson's equation with a given right hand side function
 
         Parameters
@@ -79,15 +86,16 @@ class Poisson2D:
         The solution as a Numpy array
 
         """
-        A, b = self.assemble(f=f)
+        A, b = self.assemble(f=f, g=g)
         return sparse.linalg.spsolve(A, b.ravel()).reshape((self.px.N+1, self.py.N+1))
 
 def test_poisson2d():
-        um = x*(1-x)*y*(1-y)*sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
-        f = um.diff(x, 2) + um.diff(y, 2)
-        solver = Poisson2D(1, 1, 100, 100)
-        u = solver(f)
-        error = solver.l2_error(u, um)
-        assert error < 5e-4, f"Error on finest grid too large: {error}"
+    um = x*(1-x)*y*(1-y) + x  
+    f  = um.diff(x, 2) + um.diff(y, 2)
+    g  = um  
+    solver = Poisson2D(1, 1, 100, 100)
+    u = solver(f, g=g)
+    err = solver.l2_error(u, um)
+    assert err < 5e-4, f"Error too large: {err}"
 
 
